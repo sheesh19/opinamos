ed6845b5bff349275558bd027cf82022b408689d
Sheila Leveille <sbleveille@gmail.com>
2022-11-19 14:27:47 -0700

    Add initial search

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,9 +1,13 @@
 class CompaniesController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:show]
+    skip_before_action :authenticate_user!, only: [ :index, :show ]
     before_action :set_company, only: [:edit, :show ]
 
     def index
-        @companies = Company.all
+        if params[:query].present?
+            @companies = Company.global_search(params[:query])
+        else
+            @companies = Company.all
+        end
     end
 
     def show; end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,14 @@
 class Company < ApplicationRecord
+  include PgSearch::Model
+
   belongs_to :user, optional: true
   has_many :reviews
+
+  pg_search_scope :global_search,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 
   validates :name, :email, :country, :website, presence: true
 end
